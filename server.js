9a2d3fd812384fd2ea9d33c795fbc8458c0dab0f
zeinab heidarzadeh <zeibaheidarzadeh1377@gmail.com>
2022-01-01 21:16:08 +0330

    a test server + client

--- server.js
+++ server.js
@@ -1,29 +1,85 @@
-var wss = new (require('ws')).Server({port: (process.env.PORT || 8080)});
-console.log('listening on port: ' + 8080);
+'use strict';
 
-wss.on('connection', function connection(ws, req) {
-    
-	ws.on('message', function(message) {
+const express = require('express');
+const { Server } = require('ws');
 
-        var msg = JSON.parse(message);
-        if ('topic' in msg && 'payload' in msg) {
-            if (msg.topic == 'exe') {
-                console.log('exe')
-            }
-            else if (msg.topic == 'bit') {
-                console.log('bit')
-            }
-            else {
-                ws.send(JSON.stringify(msg));
-                console.log(msg);
-            }
-        }
-        else {
-            console.log('missing topic or payload')
-        }
-        
-	});
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
 
-	console.log('new client connected!');
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
+const wss = new Server({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('close', () => console.log('Client disconnected'));
 });
+
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    client.send(new Date().toTimeString());
+  });
+}, 1000);
+
+
+
+// {
+//   "name": "server",
+//   "version": "1.0.0",
+//   "description": "remote lab project server",
+//   "main": "server.js",
+//   "scripts": {
+//     "start": "node server.js"
+//   },
+//   "engines": {
+//     "node": "12.17.x"
+//   },
+//   "repository": {
+//     "type": "git",
+//     "url": ""
+//   },
+//   "author": "zeynab heydarzadeh",
+//   "license": "ISC",
+//   "bugs": {
+//     "url": ""
+//   },
+//   "homepage": "",
+//   "dependencies": {
+//     "express": "^4.17.2"
+//   }
+// }
+
+
+
+
+// var wss = new (require('ws')).Server({port: (process.env.PORT || 8080)});
+// console.log('listening on port: ' + 8080);
+
+// wss.on('connection', function connection(ws, req) {
+    
+// 	ws.on('message', function(message) {
+
+//         var msg = JSON.parse(message);
+//         if ('topic' in msg && 'payload' in msg) {
+//             if (msg.topic == 'exe') {
+//                 console.log('exe')
+//             }
+//             else if (msg.topic == 'bit') {
+//                 console.log('bit')
+//             }
+//             else {
+//                 ws.send(JSON.stringify(msg));
+//                 console.log(msg);
+//             }
+//         }
+//         else {
+//             console.log('missing topic or payload')
+//         }
+        
+// 	});
+
+// 	console.log('new client connected!');
+
+// });
