70c1455c5e1e169c892034a9052ad92005cb1375
zeinab heidarzadeh <zeibaheidarzadeh1377@gmail.com>
2022-01-01 22:53:07 +0330

    test new client send message

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ const wss = new Server({ server });
 wss.on('connection', (ws) => {
   console.log('Client connected');
   ws.on('close', () => console.log('Client disconnected'));
-});
-
-setInterval(() => {
-  wss.clients.forEach((client) => {
-    client.send(new Date().toTimeString());
+  ws.on('message', function(message) {
+    wss.clients.forEach((client) => {
+        client.send('hi!');
+    });
   });
-}, 1000);
+
+});
