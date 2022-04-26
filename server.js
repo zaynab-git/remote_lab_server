28d4ef90357866fec46a44632b3365122c088fe1
zaynab-git <58908469+zaynab-git@users.noreply.github.com>
2022-04-26 17:59:54 +0430

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,78 @@
-'use strict';
-
 const express = require('express');
-const { Server } = require('ws');
+const WebSocket = require('ws');
+const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 80;
-const INDEX = '/index.html';
 
+const path = require('path');
 const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-const wss = new Server({ server });
-
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  wss.clients.forEach((client) => {
-    client.send('hi!');
-});
-  ws.on('close', () => console.log('Client disconnected'));
-  ws.on('message', function(message) {
-    wss.clients.forEach((client) => {
-        client.send('hi!');
+    .use(express.static(path.join(__dirname, '../flexdash/dist')))
+    .listen(PORT, '172.23.155.158', () => {
+        console.log(`Server listening on port ${PORT}!`)
     });
+
+const wss = new WebSocket.Server({ server });
+
+
+wss.on('connection', async (ws) => {
+
+
+
+
+  var modbus = require("modbus-stream");
+
+  modbus.tcp.connect(502, "172.23.154.137", { debug: "automaton-2454" }, (err, connection) => {
+      console.log('connected !')
+      if (err) throw err;
+    let preres = 0;  
+    while (true){
+      connection.readCoils({ address: 1, quantity: 1, extra: { unitId: 1 } }, (err, res) => {
+        // if (err) throw err;
+
+        if (res.response.data[0] != preres) {
+          console.log(res.response.data[0]); 
+          var data = (res.response.data[0] = 0 ? 'OFF' : 'ON');
+          ws.send(JSON.stringify({topic: '1', payload: data}))
+        }
+        
+        preres = res.response.data[0];
+
+    })
+
+    }
+      
+  });
+
+
+
+
+
+  console.log('Client connected');
+  ws.on('close', () => console.log('Client disconnected'));
+  
+  ws.on('message', function(message) {
+    let m
+    try {
+      m = JSON.parse(message)
+    } catch (e) {
+      console.log(e)
+      return
+    }
+    if (typeof m !== 'object' || typeof m.topic !== 'string' || m.payload === undefined) {
+      console.log("message is missing topic and/or payload")
+      return
+    }
+
+    ws.send(message)
+
+    if (m.topic == '1' && m.payload =='ON') {
+      // readmodbus()
+      console.log(tcpDevice.stream)
+
+    }
+
+    console.log(m)
   });
 
 });
+
+
